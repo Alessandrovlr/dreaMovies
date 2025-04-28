7be335f6643c7ba0e86a3a015e3b49bcaf7590ec
Alessandro <aleluinisilva@gmail.com>
2025-04-28 15:12:43 -0300

    remoção do css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
 
 function App() {
   return (
-    <>
-      <div><h1>ola2</h1></div>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/projeto" element={<Projeto />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 

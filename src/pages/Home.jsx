01d2a056566d3a4337c291f42d890dc52ce3f3d8
JoaoCarlosForment0 <jcformento13@gmail.com>
2025-04-28 17:38:09 -0300

    Alteração inicial da pagina Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,19 @@
 import { Link } from "react-router-dom";
-export const Home = () =>{
-    return(
-        <div id="app">
-        
-        <main class="container mx-auto px-4 py-8">
-            <div id="content" class="fade-in">
-            
-                <div class="text-center py-12">
-                    <h1 class="text-4xl font-bold mb-4">Bem vindo a DreaMovies</h1>
-                    <p class="text-xl text-gray-300 mb-8">Descubra, explore e diverta-se no mundo do cinema</p>
-                    <Link to="/filme" >Buscar Filmes</Link>
-                    {/* <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-full transition transform hover:scale-105">
+export const Home = () => {
+  return (
+    <div className="flex flex-col justify-center w-[100%] h-[100%]">
+      <main className="flex flex-col justify-center gap-[10%] items-center mx-auto px-4 py-8">
+          <h1 className="text-6xl font-bold mb-4">Bem vindo a DreaMovies</h1>
+          <p className="text-2xl text-gray-300 mb-8">
+            Descubra, explore e diverta-se no mundo do cinema
+          </p>
+          <Link to="/filme" className="p-[2.5%] font-bold decoration-0 rounded-2xl bg-[rgb(117,47,163)] text-3xl text-white">
+            Buscar Filmes -{">"}
+          </Link>
+          {/* <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-full transition transform hover:scale-105">
                         Buscar Filmes <i class="fas fa-arrow-right ml-2"></i>
                     </button> */}
-                </div>
-            </div>
-        </main>
+      </main>
     </div>
-    )
-}
+  );
+};

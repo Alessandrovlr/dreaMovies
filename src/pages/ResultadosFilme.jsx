5c27e0acf90776dd721ee81e03599ad4243d4835
Alessandro <aleluinisilva@gmail.com>
2025-05-04 05:51:09 -0300

    criação das paginas de pesquisa focada em filmes e series

---
+++ src/pages/ResultadosFilme.jsx
@@ -0,0 +1,18 @@
+import { FilmePesquisa } from "../components/filter/FilmePesquisa";
+import { useSearch } from "../contexts/SearchContext";
+
+export const ResultadosFilme = () => {
+  const { searchResults } = useSearch();
+
+  return (
+    <div>
+      <div className="flex flex-col items-center mt-10">
+        <div className="w-16 h-16 bg-gray-700 rounded-full flex items-center justify-center">
+          <i className="fas fa-search text-white text-3xl"></i>
+        </div>
+        <h1 className="text-3xl text-white font-bold">Resultados da Busca FILMES</h1>
+      </div>
+      <FilmePesquisa results={searchResults} />
+    </div>
+  );
+};
---
+++ src/pages/ResultadosSerie.jsx
@@ -0,0 +1,18 @@
+import { SeriePesquisa } from "../components/filter/SeriePesquisa";
+import { useSearch } from "../contexts/SearchContext";
+
+export const ResultadosSerie = () => {
+  const { searchResults } = useSearch();
+
+  return (
+    <div>
+      <div className="flex flex-col items-center mt-10">
+        <div className="w-16 h-16 bg-gray-700 rounded-full flex items-center justify-center">
+          <i className="fas fa-search text-white text-3xl"></i>
+        </div>
+        <h1 className="text-3xl text-white font-bold">Resultados da Busca Series</h1>
+      </div>
+      <SeriePesquisa results={searchResults} />
+    </div>
+  );
+};

33faaf84a9176ed07eaa5146311cfa791160ca52
Alessandro <aleluinisilva@gmail.com>
2025-05-03 23:05:36 -0300

    pagina serie escolhida, e correção nas variaveis da pagina serie

---
+++ src/pages/SerieEscolhido.jsx
@@ -0,0 +1,58 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { buscarTodasAsSeries } from "../services/api";
+
+export const SerieEscolhido = () => {
+  const [Series, setSeries] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    async function carregarSeries() {
+      const lista = await buscarTodasAsSeries();
+      setSeries(lista);
+    }
+
+    carregarSeries();
+  }, []);
+
+  const serie = Series.find((f) => f.id === Number(id));
+
+  if (!serie) {
+    return (
+      <div className=" flex flex-col justify-center items-center p-6 h-[87vh] gap-5">
+        <h1 className="text-white font-mono text-6xl font-bold">Error 404</h1>
+        <p className="text-white font-mono text-xl font-thin">
+          Filme não encontrado.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-row justify-center items-center gap-15">
+      <div className="h-[87vh] mt-10 ml-5">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
+          alt={serie.title}
+          className="h-[75vh] rounded-xl shadow mb-4"
+        />
+      </div>
+      <div className="flex flex-col h-[87vh] w-[50%] mt-15 gap-3">
+        <h1 className="text-3xl font-bold mb-4 mt-5">{serie.name}</h1>
+        <p className="mb-2 text-lg">
+          <span className="font-bold">Data de lançamento:</span>{" "}
+          {serie.first_air_date}
+        </p>
+        <p className="mb-2 text-lg ">
+          <span className="font-bold text-white">Nota: </span>
+          {serie.vote_average.toFixed(1) + " "}
+          <i className="fa fa-star text-yellow-500"></i>
+        </p>
+        <p className="text-gray-300 text-lg">
+          {serie.overview ? serie.overview : "Descrição não disponível"}
+        </p>
+      </div>
+    </div>
+  );
+};
